04c03ee11bcb80f2f91be831554eadd3f36d265f
Natesh <nateshmailme@gmail.com>
2020-06-10 09:26:55 +0530

    Initial Update

---
+++ app/src/main/java/com/natesh/covidcase/pojo/CovidModel.kt
@@ -0,0 +1,47 @@
+package com.natesh.covidcase.pojo
+
+data class CovidModel(
+    val cases_time_series: List<CasesTimeSery>,
+    val statewise: List<Statewise>,
+    val tested: List<Tested>
+)
+
+data class CasesTimeSery(
+    val dailyconfirmed: String,
+    val dailydeceased: String,
+    val dailyrecovered: String,
+    val date: String,
+    val totalconfirmed: String,
+    val totaldeceased: String,
+    val totalrecovered: String
+)
+
+data class Statewise(
+    val active: String,
+    val confirmed: String,
+    val deaths: String,
+    val deltaconfirmed: String,
+    val deltadeaths: String,
+    val deltarecovered: String,
+    val lastupdatedtime: String,
+    val migratedother: String,
+    val recovered: String,
+    val state: String,
+    val statecode: String,
+    val statenotes: String
+)
+
+data class Tested(
+    val individualstestedperconfirmedcase: String,
+    val positivecasesfromsamplesreported: String,
+    val samplereportedtoday: String,
+    val source: String,
+    val testpositivityrate: String,
+    val testsconductedbyprivatelabs: String,
+    val testsperconfirmedcase: String,
+    val testspermillion: String,
+    val totalindividualstested: String,
+    val totalpositivecases: String,
+    val totalsamplestested: String,
+    val updatetimestamp: String
+)
